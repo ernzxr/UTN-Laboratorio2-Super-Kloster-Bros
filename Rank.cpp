6e965dfce48bde9ee25e332e28f9f9f7739278e4
lucasarieel <luucasarieel@hotmail.com>
2024-07-17 22:19:56 -0300

    Class Rank

--- Rank.cpp
+++ Rank.cpp
@@ -1,2 +1,87 @@
-#pragma once
+
+#include <cstring>
+#include <string>
+using namespace std;
 #include "Rank.h"
+
+void cargarCadena(char* pal, int tam) {
+    int i;
+    fflush(stdin);
+    for (i = 0; i < tam; i++)
+    {
+        pal[i] = cin.get();
+        if (pal[i] == '\n') break;
+    }
+    pal[i] = '\0';
+    fflush(stdin);
+}
+
+Rank::Rank() {
+    
+}
+
+bool Rank::grabarPlayer(Rank& obj) {
+    FILE* pRank;
+    pRank = fopen("Ranking.dat", "ab");
+    if (pRank == NULL) {
+        cout << "NO SE ABRIO" << endl;
+        return false;
+    }
+
+    bool escribio = fwrite(&obj, sizeof(Rank), 1, pRank);
+    fclose(pRank);
+
+    return escribio;
+}
+
+void Rank::cargarPlayer() {
+    cout << "INGRESE SU NOMBRE " << endl;
+    cargarCadena(nombrePlayer, 29);
+    grabarPlayer(*this);
+}
+
+Rank Rank::leerPlayer(int pos) {
+    Rank obj;
+    FILE* pRank;
+    pRank = fopen("Ranking.dat", "rb");
+    if (pRank == NULL) {
+        cout << "NO SE ABRIO" << endl;
+        return obj;
+    }
+    fseek(pRank, pos * sizeof obj, SEEK_SET);
+    fread(&obj, sizeof obj, 1, pRank);
+    fclose(pRank);
+    return obj;
+}
+
+int Rank::contarRegistros() {
+    FILE* pRank;
+    pRank = fopen("Ranking.dat", "rb");
+    if (pRank == NULL) {
+        cout << "NO SE ABRIO" << endl;
+        return -1;
+    }
+    fseek(pRank, 0, SEEK_END);
+    int tam = ftell(pRank);
+    fclose(pRank);
+    return tam / sizeof(Rank);
+}
+
+void Rank::mostrarRanking() {
+    Rank obj;
+    FILE* pRank;
+    
+    pRank = fopen("Ranking.dat", "rb");
+        if (pRank == NULL) {
+            cout << "NO SE ABRIO" << endl;
+            return;
+      }
+ 
+    int cant = obj.contarRegistros();
+    for (int i = 0; i < cant; i++) {
+
+        fread(&obj, sizeof obj, 1, pRank);
+        cout << obj.nombrePlayer << " " << obj.puntajePlayer << " " << obj.intentos << endl;
+    }
+    fclose(pRank);
+}
--- Rank.h
+++ Rank.h
@@ -1,4 +1,20 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "Gameplay.h"
 class Rank
 {
+private:
+
+	char nombrePlayer[30];
+	int puntajePlayer;
+	int intentos;
+public:
+	Rank();
+	void cargarPlayer();
+	bool grabarPlayer(Rank &obj);
+	Rank leerPlayer(int pos);
+	void mostrarRanking();
+	int contarRegistros();
+	void mostrarTopTen();
 };
